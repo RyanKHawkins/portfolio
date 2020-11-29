a877b1dd0d2e10d32bfbac253b0a995567d19b40
Ryan Hawkins <rkhawkins80@hotmail.com>
2020-11-29 00:22:17 +0000

    Worked on "things"

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ var socialMediaLinks = document.querySelectorAll("a");
 var links = Array.from(socialMediaLinks)
 console.log(links[0].innerHTML)
 
+// setInterval(() => {
+//     links.forEach((link) => { link.classList.toggle("highlighted_link") })
+// }, 1000)
+
