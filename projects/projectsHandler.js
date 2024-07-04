130baf7a3c1c9b92103ae36213e1dcd8f5221194
Ryan Hawkins <rkhawkins80@hotmail.com>
2024-07-03 20:17:47 -0500

    added projects folder and projectsHandler.js

--- projects/projectsHandler.js
+++ projects/projectsHandler.js
@@ -3,7 +3,6 @@ import projects from "projects.json"
 console.log(projects);
 let projectArray = JSON.parse(projects);
 
-
 // function sortProjects() {
 //     projects = projects.sort((a, b) => a.name - b.name)
 // }
