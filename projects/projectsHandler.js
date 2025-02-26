9801f0fabdee176c4d6840fe09a20ff275d2aac0
Ryan Hawkins <rkhawkins80@hotmail.com>
2025-02-25 19:17:18 -0600

    removed projects.json

--- projects/projectsHandler.js
+++ projects/projectsHandler.js
@@ -1,10 +1,5 @@
-import projects from "projects.json"
-
-console.log(projects);
-let projectArray = JSON.parse(projects);
 
 
 // function sortProjects() {
 //     projects = projects.sort((a, b) => a.name - b.name)
 // }
-console.log(projectArray);
